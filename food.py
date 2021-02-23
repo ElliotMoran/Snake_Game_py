1696e0cfafa54d5ea9f3d68407617739dfc08ccd
ElliotMoran <adel.ahmetgaliev@yandex.ru>
2021-02-23 14:32:56 +0300

    apple cannot appear on a snake

--- food.py
+++ food.py
@@ -6,19 +6,30 @@ import settings
 class Food:
     def __init__(self):
         self.x, self.y = random.randint(
-            10, settings.width - 20), random.randint(10, settings.height - 20)
+            200, settings.width - 20), random.randint(10, settings.height - 20)
         self.color = settings.RED
 
         self.image = pygame.image.load('data/Apple.png').convert()
         self.image.set_colorkey(settings.BLACK)
-        
-        
-    def get_pos(self):
+
+    def get_pos(self) -> tuple:
         return self.x, self.y
 
-    def draw(self, screen):
+    def draw(self, screen) -> None:
         screen.blit(self.image, (self.x, self.y))
 
-    def make_new(self):
+    def make_new(self, snake_pos: list) -> None:
+        # apple cannot appear on a snake
         self.x, self.y = random.randint(
+            10, settings.width - 20), random.randint(10, settings.height - 20)
+
+        while True:
+            flag = True
+            for pos in snake_pos:
+                if self.x <= pos[0] + 15 and self.x >= pos[0] - 20:
+                    if self.y <= pos[1] + 15 and self.y >= pos[1] - 20:
+                        flag = False
+            if flag:
+                break
+            self.x, self.y = random.randint(
             10, settings.width - 20), random.randint(10, settings.height - 20)
