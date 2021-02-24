c3df37b7094e3c0a9df07684e557e1627419b642
ElliotMoran <adel.ahmetgaliev@yandex.ru>
2021-02-24 13:48:19 +0300

    removed pygame output in console

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #! pypy3
 if __name__ == '__main__':
+    import os
     import sys
     sys.path.insert(0, 'src/')
+    os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
     from game import Game
     
     game = Game()
