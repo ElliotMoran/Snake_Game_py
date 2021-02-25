8144d29b0de4bff74e0ba3d9668de7c30032d59c
ElliotMoran <adel.ahmetgaliev@yandex.ru>
2021-02-25 20:59:30 +0300

    Apples appear beautifully

--- src/food.py
+++ src/food.py
@@ -5,12 +5,13 @@ import settings
 
 class Food:
     def __init__(self):
-        self.x, self.y = random.randint(
-            200, settings.width - 20), random.randint(10, settings.height - 20)
+        self.x, self.y = None, None
         self.color = settings.RED
 
         self.image = pygame.image.load('data/Apple.png').convert()
         self.image.set_colorkey(settings.BLACK)
+
+        self.make_new([[90, 500], [75, 500], [60, 500]])  # first snake pos
 
     def get_pos(self) -> tuple:
         return self.x, self.y
@@ -20,16 +21,16 @@ class Food:
 
     def make_new(self, snake_pos: list) -> None:
         # apple cannot appear on a snake
-        self.x, self.y = random.randint(
-            10, settings.width - 20), random.randint(10, settings.height - 20)
+        self.x, self.y = random.randrange(
+            0, settings.width, 15), random.randrange(0, settings.height, 15)
 
         while True:
             flag = True
             for pos in snake_pos:
-                if self.x <= pos[0] + 15 and self.x >= pos[0] - 20:
-                    if self.y <= pos[1] + 15 and self.y >= pos[1] - 20:
+                if self.x <= pos[0] + 15 and self.x >= pos[0] - 15:
+                    if self.y <= pos[1] + 15 and self.y >= pos[1] - 15:
                         flag = False
             if flag:
                 break
-            self.x, self.y = random.randint(
-                10, settings.width - 20), random.randint(10, settings.height - 20)
+            self.x, self.y = random.randrange(
+                0, settings.width, 15), random.randrange(0, settings.height, 15)
